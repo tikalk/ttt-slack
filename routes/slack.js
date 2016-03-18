4385a6277d521dde478030d7ff610bb956b67f97
Duy K. Bui <duy@buifamily.info>
2016-03-18 17:55:58 -0400

    Drop ES2015 arrow function syntax
    To ensure compatible with legacy versions of Node JS.

--- routes/slack.js
+++ routes/slack.js
@@ -5,7 +5,7 @@ router.preferredBase = '/slack';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const db = req.app.get('db');
-  db.get('users').find({}, (err, docs) => {
+  db.get('users').find({}, function(err, docs) {
     if (err) {
       return next(err);
     }
